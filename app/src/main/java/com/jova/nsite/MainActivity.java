00ea4f9db6bdafe3de6436ce2c25f21c0fde09fe
Jovial248 <jovialjohngeorge248@gmail.com>
2019-01-30 08:53:43 +0530

    Email feature added

--- app/src/main/java/com/jova/nsite/MainActivity.java
+++ app/src/main/java/com/jova/nsite/MainActivity.java
@@ -1,5 +1,7 @@
 package com.jova.nsite;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -37,8 +39,13 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                /*
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                */
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("plain/text");
+                startActivity(Intent.createChooser(intent, ""));
             }
         });
 
